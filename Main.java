2e65ecb89d12943ce58669bc36bd57cce721ca7e
Maccode-git <62069233+Maccode-git@users.noreply.github.com>
2020-03-12 11:02:28 -0500

    Commit Inicial

---
+++ Main.java
@@ -0,0 +1,26 @@
+package clasesSeleccion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Main");
+	}
+
+	public void MetodoR1(){
+		System.out.print("Metodo Rama 1");
+	}
+
+	public void MetodoR2(){
+		System.out.print("Metodo Rama 2");
+
+	public void MetodoR3(){
+		System.out.print("Metodo Rama 3");
+
+	public void MetodoR4(){
+		System.out.print("Metodo Rama 4");
+
+	public void MetodoR5(){
+		System.out.print("Metodo Rama 5");
+	}
+	
+}
